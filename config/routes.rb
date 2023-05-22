9c36439883f9213fc4b17dabb26f9c1ab26a71d9
Charles Swaleh <charlswall@live.com>
2023-05-22 20:12:07 +0300

    Created the login authentication route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :spaces
   resources :reviews
   post "/signup", to: "users#create"
+  post "/login", to: "auth#create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
