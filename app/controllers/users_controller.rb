2c1cec2fabec0cb737278d03daa0bedea8ae2f71
Charles Swaleh <charlswall@live.com>
2023-05-22 19:13:47 +0300

    Added the methods for JWT encoding and decoding and integrated it into the User model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
+skip_before_action :authorized, only: :create
 
   def create
     user = User.create(user_params)
     if user.valid?
-      render json: user, status: :created
+      token = encode_token({user_name: user.name, user_id: user.id})
+      render json: { user: UserSerializer.new(user), jwt: token }, status: :created
     else
       render json: {error: user.errors.full_messages}, status: :unprocessable_entity
     end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :password_digest, :telephone_no, :role
+  attributes :name, :email, :telephone_no, :role
 end
