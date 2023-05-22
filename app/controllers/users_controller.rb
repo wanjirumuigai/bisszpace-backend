dcb882b53288ddfc07e7f83e141a4a02ff95c4a1
Charles Swaleh <charlswall@live.com>
2023-05-22 17:52:01 +0300

    Created the Signup route for a user and also added the validations for the user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+
+  def create
+    user = User.create(user_params)
+    if user.valid?
+      render json: user, status: :created
+    else
+      render json: {error: user.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email, :password, :telephone_no, :role)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :name, :telephone_no, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :role, inclusion: {in: ["admin", "user"]}
 end
