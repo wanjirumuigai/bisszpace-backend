957c14d09a94dee5bb181266c02a0381e9c25628
Pauline Muigai <wanjirumuigai@gmail.com>
2023-05-24 16:05:06 +0300

    edited spaces controller to handle records not found

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,20 +1,20 @@
 class SpacesController < ApplicationController
-def index
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid,
+  with: :render_unprocessable_entity_response
+  def index
   spaces = Space.all
   render json: spaces
 end
 
 def show
-  space = Space.find_by(id: params[:id])
-  if space
-    render json: space, status: :ok
-  else
-    render json: { error: "Space not found" }, status: :not_found
-  end
+  space = Space.find_by!(id: params[:id])
+  render json: space, status: :ok
+
 end
 def update
   space = Space.find(params[:id])
-  space.update(update_params)
+  space.update!(update_params)
   render json: space
     end
 def destroy
@@ -33,5 +33,14 @@ params.permit(:comment, :rating, :user_id, :space_id)
 end
 def update_params
   params.permit(:is_taken, :leased_by_id)
+end
+def render_not_found_response
+  render json: { error: "Listing not found" }, status: :not_found
+end
+def render_unprocessable_entity_response(invalid)
+  render json: {
+           errors: invalid.record.errors
+         },
+         status: :unprocessable_entity
 end
 end
