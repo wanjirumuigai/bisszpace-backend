cbe139808c9ffdc48757aca30e67249aaad5c2c2
Charles Swaleh <charlswall@live.com>
2023-05-26 13:09:26 +0300

    Updated the validations on the Space model

--- app/models/space.rb
+++ app/models/space.rb
@@ -4,7 +4,7 @@ has_many :reviews
 
 validates :name, presence: true
 validates :size, numericality: { only_integer: true}
-validates :space_type, inclusion: { in: %w(office shop warehouse),
+validates :space_type, inclusion: { in: %w(office shop warehouse Office Shop Warehouse),
     message: "%{value} is not a valid space_type. Input either office, shop or warehouse!" }
 validates :lease_cost, numericality: { only_integer: true}
 
