9577d939abb2c9b1c712d93eda4a2c88b1cce18a
Pauline Muigai <wanjirumuigai@gmail.com>
2023-05-26 11:27:53 +0300

    changed cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
 
-    origins "https://bizzspace-react.onrender.com/"
+    # origins "https://bizzspace-react.onrender.com/"
+    origins "*"
 
 
     resource "*",
