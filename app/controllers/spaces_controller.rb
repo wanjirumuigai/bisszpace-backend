89caacfa8cd4c6e28396c1192933f445c31002e4
Pauline Muigai <wanjirumuigai@gmail.com>
2023-05-28 20:57:01 +0300

    removed the available and booked methods from the skip before action

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -3,7 +3,7 @@ class SpacesController < ApplicationController
 wrap_parameters format: []
 
 
-skip_before_action :authorized, only: [:index, :welcome, :available, :booked]
+skip_before_action :authorized, only: [:index, :welcome]
 
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   rescue_from ActiveRecord::RecordInvalid,
